1e80892141ea2968e257e3db127a5d7727d6ae16
ZHANJ596 <sam.x.zhang.-nd@disney.com>
2020-07-27 16:57:12 +0800

    增加文件上传接口

--- src/main/java/com/somecom/controller/LoginController.java
+++ src/main/java/com/somecom/controller/LoginController.java
@@ -1,14 +1,26 @@
 package com.somecom.controller;
 
+import com.somecom.model.ResultVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.FileSystemUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Objects;
 
 @Api(tags = "微信登录验证")
 @RestController
@@ -16,63 +28,32 @@ import reactor.core.publisher.Mono;
 public class LoginController {
     @Autowired
     private WebClient.Builder builder;
+    @Value("${wx.file.path}")
+    private String filePath;
 
     private final static String WX_URL = "https://api.weixin.qq.com/sns/jscode2session?" +
             "appid=wx61089dda2b3d8b09&secret=1e8f235c05a63838358076eb10c96624&grant_type=authorization_code&js_code=";
 
-    @ApiOperation(value = "验证接口，传入code即可",notes = "详情参考微信API")
+    @ApiOperation(value = "验证接口，传入code即可", notes = "详情参考微信API")
     @GetMapping(path = "/finishLogin")
-    public Mono<WxResp> login(String code) {
-        if (StringUtils.hasText(code))
-            return builder.baseUrl(WX_URL + code).build().get().retrieve().bodyToMono(WxResp.class);
+    public Mono<String> login(String code) {
+        if (StringUtils.hasText(code)) {
+            return builder.baseUrl(WX_URL + code).build().get().retrieve().bodyToMono(String.class);
+        }
         return Mono.empty();
     }
 
-    private static class WxResp {
-        private String openid;
-        private String session_key;
-        private String unionid;
-        private String errcode;
-        private String errmsg;
-
-        public String getOpenid() {
-            return openid;
+    @ApiOperation(value = "上传文件接口", notes = "上传头像或者短视频")
+    @PostMapping(path = "/upload")
+    public ResultVo upload(MultipartFile file) throws IOException {
+        if (!Files.exists(Paths.get(filePath))){
+            Files.createDirectories(Paths.get(filePath));
         }
-
-        public void setOpenid(String openid) {
-            this.openid = openid;
+        if (Objects.nonNull(file) && StringUtils.hasText(file.getName())) {
+            IOUtils.copy(file.getInputStream(), new FileOutputStream(Paths.get(filePath, file.getOriginalFilename()).toFile()));
+            return ResultVo.ok();
         }
-
-        public String getSession_key() {
-            return session_key;
-        }
-
-        public void setSession_key(String session_key) {
-            this.session_key = session_key;
-        }
-
-        public String getUnionid() {
-            return unionid;
-        }
-
-        public void setUnionid(String unionid) {
-            this.unionid = unionid;
-        }
-
-        public String getErrcode() {
-            return errcode;
-        }
-
-        public void setErrcode(String errcode) {
-            this.errcode = errcode;
-        }
-
-        public String getErrmsg() {
-            return errmsg;
-        }
-
-        public void setErrmsg(String errmsg) {
-            this.errmsg = errmsg;
-        }
+        return ResultVo.err("upload failed");
     }
+
 }
