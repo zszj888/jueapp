3f0bea072fec09e6ab49221f157c6f9d53440cfc
ZHANJ596 <sam.x.zhang.-nd@disney.com>
2020-09-10 16:54:12 +0800

    Remove @EnableWebMvc annotation

--- src/main/java/com/somecom/conf/MvcConfig.java
+++ src/main/java/com/somecom/conf/MvcConfig.java
@@ -1,17 +1,22 @@
 package com.somecom.conf;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("/files/**")
-                .addResourceLocations("file:upload/");
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/files/**")
+                        .addResourceLocations("file:upload/");
+            }
+        };
     }
+
+
 }
